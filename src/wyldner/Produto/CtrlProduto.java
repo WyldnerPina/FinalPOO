5b483bfa3c56ff3764d6150b94994dbfa1e67e6a
WyldnerPina <wyldner@gmail.com>
2023-06-15 08:53:57 -0300

    bora

---
+++ src/wyldner/Produto/CtrlProduto.java
@@ -0,0 +1,133 @@
+package wyldner.Produto;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import wyldner.Vendas.CtrlVendas;
+
+public class CtrlProduto {
+	private LongProperty codProd = new SimpleLongProperty(0);
+	private StringProperty nome = new SimpleStringProperty("");
+	private StringProperty desc = new SimpleStringProperty("");
+	private DoubleProperty precoUnit = new SimpleDoubleProperty(0);
+	private IntegerProperty qntProd = new SimpleIntegerProperty(0);
+	
+	private ObservableList<Produto> listaProd = FXCollections.observableArrayList();
+	private IProdDAO pDao;
+//===================================================================================================================================
+	public CtrlProduto() throws ClassNotFoundException, SQLException {
+		pDao = new ProdDAO();
+	}
+	
+	//---------------------------------------------------------------------------------
+	
+	public void limpar() {
+		System.out.println("limpando");
+		codProd.set(0);
+		nome.set("");
+		desc.set("");
+		precoUnit.set(0);
+		qntProd.set(0);
+	}
+	
+	//---------------------------------------------------------------------------------
+
+	public void vender(Produto p) throws SQLException, ClassNotFoundException { 
+		CtrlVendas cv = new CtrlVendas();
+		int qnt = Integer.parseInt(JOptionPane.showInputDialog("qntos deseja vender?"));
+		cv.adicionar(p, qnt);
+		
+		if(p.getQntProd() - qnt > 0) {
+			System.out.println(p.getCodProduto());
+			p.setQntProd(p.getQntProd() - qnt);
+			pDao.atualizar(p.getCodProduto(), p);
+			limpar();
+			pesquisar();
+		}else {
+			listaProd.remove(p);
+			pDao.remover(p.getCodProduto());
+		}
+	}
+	
+	public void fromEntity(Produto p) { 
+		codProd.set(p.getCodProduto());
+		nome.set(p.getNome());
+		desc.set(p.getDescricao());
+		precoUnit.set(p.getPrecoUnit());
+		qntProd.set(p.getQntProd());
+	}
+	
+	public void adicionar() throws SQLException { 
+		if (codProd.get() == 0) {
+			Produto p = new Produto();
+			p.setCodProduto(codProd.get());
+			p.setNome(nome.get());
+			p.setDescricao(desc.get());
+			p.setPrecoUnit(precoUnit.get());
+			p.setQntProd(qntProd.get());
+			
+			p = pDao.adicionar(p);
+			listaProd.add(p);
+			
+		} else { 
+			Produto p = new Produto();
+			p.setCodProduto(codProd.get());
+			p.setNome(nome.get());
+			p.setDescricao(desc.get());
+			p.setPrecoUnit(precoUnit.get());
+			p.setQntProd(qntProd.get());
+			
+			System.out.println(p);
+			
+			for (int i = 0; i < listaProd.size(); i++) { 
+				Produto prod = listaProd.get(i);
+				if (prod.getCodProduto() == codProd.get()) { 
+					listaProd.set(i, p);
+				}
+			}
+			pDao.adicionar(p);
+		}
+		limpar();
+		pesquisar();
+	}
+	
+	public void pesquisar() throws SQLException { 
+		listaProd.clear();
+		List<Produto> lst = pDao.procurarNome(nome.get());
+		listaProd.addAll(lst);		
+	}
+	
+	/* FALTA FAZER O PESQUISAR POR COD */
+	
+//===================================================================================================================================
+	public final ObservableList<Produto> getListaProd() {
+		return listaProd;
+	}
+	public final LongProperty getCodProd() {
+		return codProd;
+	}
+	public final StringProperty getNome() {
+		return nome;
+	}
+	public final StringProperty getDesc() {
+		return desc;
+	}
+	public final DoubleProperty getPreco() {
+		return precoUnit;
+	}
+	public final IntegerProperty getQntProd() {
+		return qntProd;
+	}
+}
